6f43b4b6bc0f09f460146f6384e27ec01941fc4f
Yashika118 <kumariyashika78@gmail.com>
2024-12-14 20:00:54 +0530

    login logout udpateProfile  checkAuth and middleware for protectRoute  are working

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,15 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
+// signup
 export const signup=async (req,res)=>{
     const {fullName,email,password}=req.body;
    try {
+    if(!fullName || !email || !password){
+        return res.status(400).json({message:"All fields are required"});
+    }
     if(password.length<6){
         return res.status(400).json({message:"Password must be atleast 6 character"});
     }
@@ -45,10 +50,81 @@ export const signup=async (req,res)=>{
    }
 }
 
-export const login=(req,res)=>{
-    res.send("login route");
+
+// login
+export const login=async(req,res)=>{
+    const {email,password}=req.body;
+    try {
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+        const isPasswordCorrect=await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic,
+            
+        });
+
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({message:"Internal Server error"});
+    }
 }
 
+
+// logout
 export const logout=(req,res)=>{
-    res.send("logout route");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal Server error"});
+    }
 }
+
+
+// update profile pic
+export const updateProfile=async(req,res)=>{
+    try {
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+        if(!profilePic){
+            return res.status(404).json({message:"Profile pic is required"});
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const updateUser=await User.findByIdAndUpdate(
+            userId,{
+              profilePic:uploadResponse.secure_url,
+            },
+            {new:true},
+        )
+
+        res.status(200).json(updateUser);
+
+    } catch (error) {
+        console.log("error in updated profile:",error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
+
+
+// checkAuth
+export const checkAuth=async(req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message:"Internal Server Error"});   
+    }
+} 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {signup,login,logout} from "../controllers/auth.controller.js";
+import {signup,login,logout, updateProfile, checkAuth} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 const router=express.Router();
 
 // router.post("/signup",(req,res)=>{
@@ -14,5 +15,10 @@ router.post("/signup",signup);
 router.post("/login",login);
 
 router.post("/logout",logout);
+
+// for protectRoute we use file auth.middleware.js
+router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check",protectRoute,checkAuth);
 
 export default router;
