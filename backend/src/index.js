c90a554ff22230d41faf19c7bc6bce3a5a8a6ee0
Yashika118 <kumariyashika78@gmail.com>
2024-12-15 17:01:15 +0530

    navbar, loginPage, SignUpPage are working

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,22 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";  // to handle request from frontend
 
 const app=express();
 dotenv.config();
 const port=process.env.PORT;
 
 
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+
+
+// to handle request from frontend
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
